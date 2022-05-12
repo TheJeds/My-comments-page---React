822c357b79d07c5086d5535b0e96fe40e04aab01
TheJeds <diegogalaxy15@gmail.com>
2022-05-12 12:56:32 -0500

    component implementation

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import '../css/Comments.css';
 
-function Comments() {
+function Comments(props) {
 	return (
 		<div className='container-comment'>
-			<img className='image-comment' src={require('../images/comment-emma.png')} alt='Image Emma' />
+			<img className='image-comment' src={require(`../images/comment-${props.image}.png`)} alt="image" />
 			<div className='container-text-comment'>
-				<p className='name-comment'>Emma Bostian in Sweden</p>
-				<p className='job-comment'>Software Engineer at Spotify</p>
-				<p className='text-comment'>"I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."</p>
+				<p className='name-comment'><strong>{props.name}</strong> in {props.country}</p>
+				<p className='job-comment'>str{props.job} in <strong>{props.company}</strong></p>
+				<p className='text-comment'>"{props.comment}"</p>
 			</div>
 		</div>
 	);
