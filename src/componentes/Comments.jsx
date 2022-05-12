661e18029ba2a99e5691efe842f7f6b9c882108c
TheJeds <diegogalaxy15@gmail.com>
2022-05-12 00:41:55 -0500

    Initial commit

---
+++ src/componentes/Comments.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import React from 'react';
+
+function Comments() {
+    return (
+        <div className="container-comment">
+            <img className="image-comment" src={require('../images/comment-emma.png')} alt='Image Emma' />
+        </div>
+    );
+}
